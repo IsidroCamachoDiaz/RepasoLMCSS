8cb0ef0ae0b3462eed7d8981f231e93dff207daf
DESKTOP-5AFE429\Isidro Camacho <isidroswicth@gmail.com>
2023-05-12 13:40:23 +0200

    12-5-2023v2

--- RepasoLM/src/main/webapp/script/script.js
+++ RepasoLM/src/main/webapp/script/script.js
@@ -1,10 +1,23 @@
 function enviarDatos(form){
+	var dni=form.dni.value;
 	var nombre= form.nombre.value;
 	var apellidos=form.apellidos.value;
 	var edad=form.edad.value;
-	localStorage.setItem("nombre-"+nombre,nombre);
-	localStorage.setItem("apellidos-"+nombre,apellidos);
-	localStorage.setItem("edad-"+nombre,edad);
+	localStorage.setItem("dni-"+dni,dni);
+	localStorage.setItem("nombre-"+dni,nombre);
+	localStorage.setItem("apellidos-"+dni,apellidos);
+	localStorage.setItem("edad-"+dni,edad);
+}
+function buscar(form){
+	var dni=form.dniB.value;
+	var nombre= localStorage.getItem("nombre-"+dni);
+	var apellidos=localStorage.getItem("apellidos-"+dni);
+	var edad=localStorage.getItem("edad-"+dni);
+	if(nombre==apellidos)
+		alert("No se encontro a la persona")
+	else
+		document.writeln("Nombre: "+nombre+" Apellidos: "+apellidos+" Edad: "+edad);
+}
+function enviarP(){
 	location.href ='http://localhost:8080/RepasoLM/presenta.html';
-	document.writeln("Nombre: "+nombre+" Apellidos: "+apellidos+" Edad: "+edad);
 }

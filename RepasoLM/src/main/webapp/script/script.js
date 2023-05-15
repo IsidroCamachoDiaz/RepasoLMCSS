cecdb2bfda70338e33f19f22a966ef4d95f16d67
DESKTOP-5AFE429\Isidro Camacho <isidroswicth@gmail.com>
2023-05-15 09:06:44 +0200

    15-5-2023

--- RepasoLM/src/main/webapp/script/script.js
+++ RepasoLM/src/main/webapp/script/script.js
@@ -1,22 +1,29 @@
-function enviarDatos(form){
+class persona{
+	constructor (dni,nombre,apellidos,edad){
+		this.dni=dni;
+		this.nombre=nombre;
+		this.apellidos=apellidos;
+		this.edad=edad;
+	}
+}
+
+function enviarDatos(form){	
 	var dni=form.dni.value;
 	var nombre= form.nombre.value;
 	var apellidos=form.apellidos.value;
 	var edad=form.edad.value;
-	localStorage.setItem("dni-"+dni,dni);
-	localStorage.setItem("nombre-"+dni,nombre);
-	localStorage.setItem("apellidos-"+dni,apellidos);
-	localStorage.setItem("edad-"+dni,edad);
+	var p1=new persona(dni,nombre,apellidos,edad);
+	localStorage.setItem("p-"+dni,JSON.stringify(p1));
 }
 function buscar(form){
 	var dni=form.dniB.value;
-	var nombre= localStorage.getItem("nombre-"+dni);
-	var apellidos=localStorage.getItem("apellidos-"+dni);
-	var edad=localStorage.getItem("edad-"+dni);
-	if(nombre==apellidos)
+	var valor=localStorage.getItem("p-"+dni);
+	var p2= JSON.parse(valor);
+	
+	if(p2.nombre==p2.apellidos)
 		alert("No se encontro a la persona")
 	else
-		document.writeln("Nombre: "+nombre+" Apellidos: "+apellidos+" Edad: "+edad);
+		document.writeln("Nombre: "+p2.nombre+" Apellidos: "+p2.apellidos+" Edad: "+p2.edad);
 }
 function enviarP(){
 	location.href ='http://localhost:8080/RepasoLM/presenta.html';
